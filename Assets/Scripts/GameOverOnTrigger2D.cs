75438b424ee062426d3c4d839d0e9838f983500a
kobi075@gmail.com <kobi075@gmail.com>
2024-11-28 20:44:47 +0200

    fix linter

--- Assets/Scripts/GameOverOnTrigger2D.cs
+++ Assets/Scripts/GameOverOnTrigger2D.cs
@@ -6,8 +6,8 @@ public class GameOverOnTrigger2D : MonoBehaviour
 {
     [Tooltip("Every object tagged with this tag will trigger game over")]
     [SerializeField] string triggeringTag;
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && enabled) {
+    private void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == triggeringTag && enabled){
             Debug.Log("Game over!");
             Application.Quit();
             UnityEditor.EditorApplication.isPlaying = false;  // Error on editor 2021.3
